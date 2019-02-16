4f852fce1bbb309bac64c93d9a1cd08840991e6f
Erfan Alvand <iamerfanalvand@gmail.com>
2019-02-17 00:26:14 +0330

    Change color in console

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ $('.button').on("click",function(){
     $('.myname, .mytitle, .button, img').addClass('bounceOut');
 })
 
-console.log("%cMy GitHub", "font: 3 sans-serif; Color: blue;");
-console.log("%chttps://github.com/erfanalvand", "font: 1 sans-serif; Color: gray;");
+console.log("%cMy GitHub:","font: 2em sans-serif; color: #2fc5ff;")
+console.log("%chttps://github.com/erfanalvand", "font: 1em sans-serif; Color: #cacaca;");
